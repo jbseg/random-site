a8a17d0391e58660e527539f506d035612dd952b
Joshua Segal <jbseg@umich.edu>
2019-03-11 15:52:09 -0400

    added something to init.py

--- randomsite/__init__.py
+++ randomsite/__init__.py
@@ -9,3 +9,6 @@ app = flask.Flask(__name__)
 
 import randomsite.api # noqa: E402 pylint: disable=wrong-import-position
 import randomsite.views # noqa: E402 pylint: disable=wrong-import-position
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0")
